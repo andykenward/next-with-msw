c1011132cdeab80feb0a8764cb6bcfb1d4d4033f
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2022-11-23 17:25:00 +0000

    chore: code formatting

--- with-msw-pages/mocks/index.ts
+++ with-msw-pages/mocks/index.ts
@@ -1,9 +1,9 @@
-if (typeof window === "undefined") {
-  const { server } = await import("./server")
-  server.listen()
+if (typeof window === 'undefined') {
+  const { server } = await import('./server')
+  server.listen({ onUnhandledRequest: 'bypass' })
 } else {
-  const { worker } = await import("./browser")
-  worker.start()
+  const { worker } = await import('./browser')
+  worker.start({ onUnhandledRequest: 'bypass' })
 }
 
 export {}
